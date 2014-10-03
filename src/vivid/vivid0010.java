0a9ea6797d143de071f3c12e3385a2293ea27bf9
aibara <aibara@cmedia.co.jp>
2014-10-03 11:25:42 +0900

    commit2 (add yellow method)

--- src/vivid/vivid0010.java
+++ src/vivid/vivid0010.java
@@ -6,11 +6,15 @@ public class vivid0010 {
 	public void red(){
 		colors = 1;
 	}
-	
+
 	public void blue(){
 		colors = 2;
 	}
-	
+
+	public void yellow(){
+		colors = 3;
+	}
+
 	public void main(){
 	}
 }
